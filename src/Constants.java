336e409ada80d6fe3057f4a179664292fbc5a6a5
User <dg21stew@gmail.com>
2022-11-01 12:06:49 -0700

    Added Constants class

---
+++ src/Constants.java
@@ -0,0 +1,15 @@
+public class Constants {
+
+    private Constants(){
+
+    }
+    //Variables that don't change
+    public static final int NUMBER_OF_MONTHS_IN_A_YEAR = 12;
+
+    public static final int NUMBER_OF_DAYS_IN_A_WEEK = 7;
+
+    public static final String MY_NAME = "Bob";
+
+
+
+}
--- src/Constructors.java
+++ src/Constructors.java
@@ -8,6 +8,7 @@ public class Constructors {
         System.out.println(car.year);
         System.out.println(car.make);
         System.out.println(car.model);
+        System.out.println(car.year + " " + car.make + " " + car.model);
 
         //space
         System.out.println("\n");
@@ -16,13 +17,24 @@ public class Constructors {
         System.out.println(car1.year);
         System.out.println(car1.make);
         System.out.println(car1.model);
+        System.out.println(car1.year + " " + car1.make + " " + car1.model);
 
+        //space
         System.out.println("\n");
 
         Car car2 = new Car(1999, "Nissan", "Skyline R34");
         System.out.println(car2.year);
         System.out.println(car2.make);
         System.out.println(car2.model);
+
+        System.out.println(car2.year + " " + car2.make + " " + car2.model);
+
+        //space
+        System.out.println("\n");
+
+        //Constructor not visible because it is private but static fields can be used
+        System.out.println(Constants.MY_NAME);
+
 
     }
 }
